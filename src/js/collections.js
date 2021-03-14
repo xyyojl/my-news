7b2916037722e94740360404069a5d226ed42c98
xyyojl <m13631897256@163.com>
2021-03-14 16:48:46 +0800

    收藏页面复用列表组件

--- src/js/collections.js
+++ src/js/collections.js
@@ -1,11 +1,15 @@
 import './imports';
+import NewsList from '../components/NewsList';
 import Header from '../components/Header';
 
 ;(function(doc){
     const oApp = doc.querySelector('#app');
+    const followedList = JSON.parse(localStorage.getItem('followedList') || '[]');
+    let oListWrapper = null;
 
     const init = () => {
         render();
+        bindEvent();
     }
 
     function render() {
@@ -16,8 +20,40 @@ import Header from '../components/Header';
             showRightIcon: false
         });
 
-        oApp.innerHTML += headerTpl;
+        // 判断有收藏数，才显示
+        if(followedList.length) {
+            const listWrapperTpl = NewsList.wrapperTpl(44);
+            oApp.innerHTML += (headerTpl + listWrapperTpl);
+            oListWrapper = oApp.querySelector('.news-list');
+            renderList(followedList);
+        } else {
+            // 没有数据，显示提示。使用 NoDataTip 组件
+            
+        }
+
     };
+
+    function renderList(data) {
+        const newsListTpl = NewsList.tpl({
+            data,
+            pageNum: -1
+        });
+        oListWrapper.innerHTML += newsListTpl;
+        NewsList.imgShow();
+    };
+
+    function bindEvent() {
+        followedList.length && NewsList.bindEvent(oListWrapper, setCurrentNews);
+    }
+
+    // 找到对应的数据，并存储到 localStorage
+    function setCurrentNews(options) {
+        console.log(options);
+        const { idx } = options;
+        // understand
+        const currentNews = followedList[idx];
+        localStorage.setItem('currentNews', JSON.stringify(currentNews));
+    }
 
     init();
 
